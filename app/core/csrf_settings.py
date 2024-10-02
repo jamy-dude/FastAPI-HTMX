0e9bd70a29bf0cf470742de975331ac63d8824dd
jamy-dude <jamesstanford2002@gmail.com>
2024-10-02 21:17:00 +0500

    Initial commit

--- app/core/csrf_settings.py
+++ app/core/csrf_settings.py
@@ -1,16 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
-
+from typing import Literal
 
 class CsrfSettings(BaseSettings):
-    secret_key: str = Field(validation_alias="CSRF_SECRET_KEY")
-    cookie_samesite: str = Field(validation_alias="COOKIE_SAMESITE")
-    cookie_secure: bool = Field(validation_alias="COOKIE_SECURE")
+    secret_key: str = Field(..., validation_alias="CSRF_SECRET_KEY")
+    cookie_samesite: Literal["strict", "lax", "none"] = Field("lax", validation_alias="COOKIE_SAMESITE")
+    cookie_secure: bool = Field(False, validation_alias="COOKIE_SECURE")
 
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
     )
 
-
-crsf_settings = CsrfSettings()
-print(crsf_settings.secret_key)
+csrf_settings = CsrfSettings()
+print(csrf_settings.secret_key)
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from loguru import logger
+from app.app import app  # Add this line
 
 if __name__ == "__main__":
     import uvicorn
 
     logger.info("Starting uvicorn in reload mode")
     uvicorn.run(
-        "app.app:app",
+        app,  # Change this line
         host="127.0.0.1",
         reload=True,
         port=8080,
